7f8b77aa56a41e0ce5cc7b1760364b6c778be93c
Azizi <190982731+Azizi-X@users.noreply.github.com>
2026-02-13 17:04:31 -0500

    .

--- RemoteAuth.go
+++ RemoteAuth.go
@@ -1,48 +1,32 @@
 package RemoteAuth
 
 import (
-	"bytes"
 	"context"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"maps"
-
 	"sync"
 	"time"
 
-	http2 "net/http"
-
 	"github.com/Azizi-X/utils/debug"
 	http "github.com/bogdanfinn/fhttp"
-	tls_client "github.com/bogdanfinn/tls-client"
-	"github.com/bogdanfinn/tls-client/profiles"
+	"github.com/bogdanfinn/websocket"
 	"github.com/buger/jsonparser"
-	"github.com/gorilla/websocket"
 )
 
-const WSS_URL = "wss://remote-auth-gateway.discord.gg/?v=2"
-
 var (
-	DefaultClient, _ = tls_client.NewHttpClient(nil, tls_client.WithClientProfile(profiles.Chrome_131), tls_client.WithTimeoutSeconds(120))
-
-	Headers = http.Header{
-		"user-agent":   []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) discord-ptb/1.0.1156 Chrome/134.0.6998.205 Electron/35.3.0 Safari/537.36"},
-		"content-type": []string{"application/json"},
-	}
-
 	ErrCaptchaRequired = errors.New("captcha required")
 )
 
 type RemoteOptions struct {
 	OnFingerprint func(fingerprint string) error
 	OnExchange    func(token string) error
+	OnHTTP        func(method string, url string, data any) (*http.Response, []byte, error)
+	OnSocket      func(ctx context.Context, u string) (*websocket.Conn, *http.Response, error)
 }
 
 type Remote struct {
@@ -55,10 +39,9 @@ type Remote struct {
 	pkixBytes    []byte
 	heartbeater  *time.Ticker
 	conn         *websocket.Conn
-	dialer       *websocket.Dialer
-	headers      http.Header
 	heartbeatAck bool
 	opts         RemoteOptions
+	url          string
 
 	Fingerprint string
 	Token       string
@@ -121,32 +104,17 @@ func (r *Remote) _connect() error {
 		return r.Verbose("already connected")
 	}
 
-	if r.headers == nil {
-		r.headers = http.Header{
-			"Origin": []string{"https://discord.com"},
-		}
-	}
-
-	if r.dialer == nil {
-		r.dialer = &websocket.Dialer{
-			HandshakeTimeout: 30 * time.Second,
-		}
-	}
-
 	r.Verbose("[CONNECTING]")
 
 	ctx, cancel := context.WithTimeout(r.ctx, 30*time.Second)
 	defer cancel()
 
-	headers := make(http2.Header)
-	maps.Copy(headers, r.headers)
-
-	conn, _, err := r.dialer.DialContext(ctx, WSS_URL, headers)
+	conn, _, err := r.opts.OnSocket(ctx, r.url)
 	if err != nil {
 		return r.Verbose("%s", err.Error())
 	}
 
-	r.Verbose("[CONNECTED] %s", WSS_URL)
+	r.Verbose("[CONNECTED] %s", r.url)
 
 	r.conn = conn
 
@@ -180,7 +148,6 @@ func (r *Remote) Connect() error {
 
 	for r.ctx.Err() == nil {
 		_, raw, err := r.conn.ReadMessage()
-
 		if err != nil {
 			if r.handleErr(err) {
 				continue
@@ -231,18 +198,14 @@ func (r *Remote) Connect() error {
 			}
 
 			r.Fingerprint = fingerprint
-
 			if r.opts.OnFingerprint != nil {
 				if err := r.opts.OnFingerprint(fingerprint); err != nil {
 					return err
 				}
 			}
-
 		case "pending_login":
 			ticket, _ := jsonparser.GetString(raw, "ticket")
-
 			token, err := r.exchange(ticket)
-
 			if err != nil {
 				return err
 			}
@@ -265,41 +228,27 @@ func (r *Remote) Connect() error {
 }
 
 func (r *Remote) exchange(ticket string) (token string, err error) {
-	data, _ := json.Marshal(map[string]string{
+	data := map[string]string{
 		"ticket": ticket,
-	})
+	}
 
-	req, err := http.NewRequest("POST", "https://ptb.discord.com/api/v9/users/@me/remote-auth/login", bytes.NewBuffer(data))
+	request, body, err := r.opts.OnHTTP("POST", "/users/@me/remote-auth/login", data)
 	if err != nil {
 		return "", err
 	}
 
-	maps.Copy(req.Header, Headers)
-
-	resp, err := DefaultClient.Do(req)
-	if err != nil {
-		return "", r.Verbose("%s", err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	body, _ := io.ReadAll(resp.Body)
-
 	site_key, _ := jsonparser.GetString(body, "captcha_sitekey")
-
-	if resp.StatusCode == 400 && site_key != "" {
-		r.Verbose("[CAPTCHA] %d", resp.StatusCode)
+	if request.StatusCode == 400 && site_key != "" {
+		r.Verbose("[CAPTCHA] %d", request.StatusCode)
 		return "", ErrCaptchaRequired
 	}
 
-	if resp.StatusCode != 200 {
-		return "", r.Verbose("status code: %d | %s", resp.StatusCode, string(body))
+	if request.StatusCode != 200 {
+		return "", r.Verbose("status code: %d | %s", request.StatusCode, string(body))
 	}
 
 	encrypted, _ := jsonparser.GetString(body, "encrypted_token")
-
 	decoded, _ := base64.StdEncoding.DecodeString(encrypted)
-
 	decrypted, _ := rsa.DecryptOAEP(sha256.New(), nil, r.privateKey, decoded, nil)
 
 	return string(decrypted), nil
@@ -313,31 +262,30 @@ func (r *Remote) Verbose(msg string, formats ...any) error {
 	return fmt.Errorf(msg, formats...)
 }
 
-func (r *Remote) WithDialer(dialer websocket.Dialer) *Remote {
-	r.dialer = &dialer
-	return r
-}
-
-func (r *Remote) WithHeaders(headers http.Header) *Remote {
-	r.headers = headers
-	return r
-}
-
 func (r *Remote) WithLogger(logger *debug.Logger) *Remote {
 	r.logger = logger
 	return r
 }
 
-func NewRemote(ctx context.Context, opts *RemoteOptions) *Remote {
+func validateOptions(url string, opts RemoteOptions) {
+	if opts.OnHTTP == nil {
+		panic("[RemoteAuth] OnHTTP can not be nil")
+	} else if opts.OnSocket == nil {
+		panic("[RemoteAuth] ConnectSocket can not be nil")
+	} else if url == "" {
+		panic("[RemoteAuth] url can not be empty")
+	}
+}
+
+func NewRemote(ctx context.Context, opts RemoteOptions, url string) *Remote {
 	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
 
-	if opts == nil {
-		opts = &RemoteOptions{}
-	}
+	validateOptions(url, opts)
 
 	return &Remote{
 		ctx:        ctx,
 		privateKey: privateKey,
-		opts:       *opts,
+		opts:       opts,
+		url:        url,
 	}
 }
